ec089e168b3653f6323dadb517f1ea0ad63454ce
Firealarm-freak <mike.stegne.host@gmail.com>
2016-04-26 13:21:22 -0400

    Finished the improved version of the user interface controller, yet it is still untested. Next commit should be the first decently working version of this clock. If I remember I'll make sure to put the hex file under the releases tab.

--- Digital Clock.X/Main.c
+++ Digital Clock.X/Main.c
@@ -2,8 +2,8 @@
  *  Digital Clock - A simple digital clock project to help me learn PIC  *
  *  Created by mikemadealarms on January 6, 2016 at 4:38 PM              *
  * --------------------------------------------------------------------- *
- *  Last modified by mikemadealarms on April 26, at 1:03 PM              *
- *  Last modification made was: Update display for new prototype         *
+ *  Last modified by mikemadealarms on April 26, at 1:19 PM              *
+ *  Last modification made was: Update the user interface and clean up   *
  *************************************************************************/
 
 #include <xc.h>
@@ -35,6 +35,9 @@
 /*******************
  *  Configuration  *
  *******************/
+
+//User Interface Configuration
+#define TIME_SET_DELAY 0xFF;  //The lower the value, the faster the numbers will increment while using the buttons to set the time
 
 //Display Mapping
 unsigned char DISPLAY_MAPPING[] = {
@@ -71,15 +74,18 @@ unsigned char displayValue[] = {0x00, 0x00, 0x00, 0x00, 0x00};  //Holds the valu
 //Setup Function, configures MCU modules, initializes variables, and set IO registers
 void setup() {
     //Timing Related Registers
-    OSCCON = 0x68;      //Set the internal oscillator to run at 125kHz
-    OPTION_REG = 0x5F;  //Enable the internal weak pull-ups on PORTB and set the Timer0 module to run with no pre-scaler from the internal oscillator
-    TMR1H = 0xFF;       //Set the highest byte of the Timer1 module counter
-    TMR1L = 0xFF;       //Set the lowest byte of the Timer1 module counter
-    T1CON = 0x85;       //Enable the Timer1 module and set it to use the internal low frequency oscillator
+    OSCCON = 0x68;         //Set the internal oscillator to run at 125kHz
+    OPTION_REG = 0x5F;     //Enable the internal weak pull-ups on PORTB and set the Timer0 module to run with no pre-scaler from the internal oscillator
+    TMR1H = 0xFF;          //Set the highest byte of the Timer1 module counter
+    TMR1L = 0xFF;          //Set the lowest byte of the Timer1 module counter
+    T1CON = 0x85;          //Enable the Timer1 module and set it to use the internal low frequency oscillator
+    T2CON = 0x07;          //Enable the Timer2 module and set the prescaler to count on every 64th clock cycle
+    PR2 = TIME_SET_DELAY;  //Set the period register of Timer2 used to determine when an interrupt should occur to the value of the constant TIME_SET_DELAY 
     
     //Interrupt Related Registers
     INTCON = 0xE0;  //Enable interrupts, along with peripheral and Timer0 module overflow
-    PIE1 = 0x01;    //Enable the Timer1 module overflow interrupt
+    PIR1 = 0x00;    //Clear all the interrupt flags to prevent any false interrupts
+    PIE1 = 0x03;    //Enable the Timer1 module overflow interrupt and the Timer2 module match interrupt
     
     //IO Port Related Registers
     TRISA = 0x00;   //Set all of PORTA to be outputs
@@ -190,5 +196,34 @@ void interrupt onInterrupt() {
         
         //Multiplex the next digit of the display
         multiplexDisplay();  //Multiplex the next digit of the display
+    }
+    
+    //Proceed only if the Timer1 module match flag has been thrown
+    if (TMR2IF) {
+        TMR2IF = 0x00;  //Clear the Timer2 module match interrupt flag to prevent false interrupts
+        
+        //Check to see if the minutes button is being held down
+        if (!RB4 && RB5) {
+            timeMinutes++;  //Increment the minutes counter by 1
+            
+            //Check to see if the minutes value is over an hour
+            if (timeMinutes == 60) {
+                timeMinutes = 0;  //Reset the minutes counter back to 0
+            }
+            
+            updateDisplay();  //Update the values on the display to reflect the newly set time
+        }
+        
+        //Check to see if the hours button is being held down
+        if (RB4 && !RB5) {
+            timeMinutes++;  //Increment the hours counter by 1
+            
+            //Check to see if the hours value is over a day
+            if (timeMinutes == 24) {
+                timeHours = 0;  //Reset the hours counter back to 0
+            }
+            
+            updateDisplay();  //Update the values on the display to reflect the newly set time
+        }
     }
 }
