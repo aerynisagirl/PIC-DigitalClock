2017bcad882e18864452c5f51ed4d53cba03f255
Firealarm-freak <mike.stegne.host@gmail.com>
2016-04-26 13:04:50 -0400

    Update the multiplexing routine used to drive the display to correctly drive the display setup on the new prototype, and also prepare to make a major change to the user interface setup that should be a lot more efficient than the previous method that was used.

--- Digital Clock.X/Main.c
+++ Digital Clock.X/Main.c
@@ -2,8 +2,8 @@
  *  Digital Clock - A simple digital clock project to help me learn PIC  *
  *  Created by mikemadealarms on January 6, 2016 at 4:38 PM              *
  * --------------------------------------------------------------------- *
- *  Last modified by mikemadealarms on April 23, at 12:35 AM             *
- *  Last modification made was: Added in functionality to set the time   *
+ *  Last modified by mikemadealarms on April 26, at 1:03 PM              *
+ *  Last modification made was: Update display for new prototype         *
  *************************************************************************/
 
 #include <xc.h>
@@ -36,9 +36,6 @@
  *  Configuration  *
  *******************/
 
-//Clock Configuration
-#define CLOCK_SET_SPEED 500  //The speed at which the time frame that is being set increments, provides a delay to make use of the clock easier
-
 //Display Mapping
 unsigned char DISPLAY_MAPPING[] = {
                                    0xB7,  //Digit mapping of 0
@@ -66,9 +63,6 @@ unsigned char timeHours = 0;    //Tracks the current hour of the day and then re
 //Display Multiplexing Variables
 unsigned char displayDigit = 0;                                 //Represents the current digit in the display that is being multiplexed
 unsigned char displayValue[] = {0x00, 0x00, 0x00, 0x00, 0x00};  //Holds the values that get written to the IO ports controlling the segment for each display that represent a number
-
-//Clock Variables
-unsigned int setTimer = 0;  //Used as a counter to provide a delay in between time frame increments while setting the time of the clock
 
 /***********
  *  Setup  *
@@ -103,8 +97,6 @@ void setup() {
     timeSeconds = 0;  //Clear the variable used for tracking seconds so it starts at the beginning
     timeMinutes = 0;  //Clear the variable used for tracking minutes so it starts at the beginning
     timeHours = 0;    //Clear the variable used for tracking hours so it starts at the beginning
-    
-    setTimer = CLOCK_SET_SPEED - 1;  //Set the counter used to add a delay between increments of a time frame when setting the clocks time to the end of the cycle to start immediately
 }
 
 /****************
@@ -167,10 +159,10 @@ void updateDisplay() {
     }
     
     //Set the values of the digits on the display to be the current time
-    displayValue[1] = DISPLAY_MAPPING[timeHours % 100 / 10];
-    displayValue[2] = DISPLAY_MAPPING[timeHours % 10];
-    displayValue[3] = DISPLAY_MAPPING[timeMinutes % 100 / 10];
-    displayValue[4] = DISPLAY_MAPPING[timeMinutes % 10];
+    displayValue[1] = DISPLAY_MAPPING[timeHours % 10];          //Set the first digit of the display to display the tens place value of the current hour
+    displayValue[2] = DISPLAY_MAPPING[timeHours % 100 / 10];    //Set the second digit of the display to display the ones place value of the current hour
+    displayValue[3] = DISPLAY_MAPPING[timeMinutes % 100 / 10];  //Set the third digit of the display to display the tens place value of the current minute
+    displayValue[4] = DISPLAY_MAPPING[timeMinutes % 10];        //Set the fourth digit of the display to display the ones place value of the current minute
 }
 
 /****************
@@ -198,52 +190,5 @@ void interrupt onInterrupt() {
         
         //Multiplex the next digit of the display
         multiplexDisplay();  //Multiplex the next digit of the display
-        
-        //Proceed only if the minutes button is being pressed
-        if (!RB4 && RB5) {
-            setTimer++;  //Increment the timer used to delay each increment of the minutes to make user interface much easier
-            
-            //Check to see if the timer has overflowed
-            if (setTimer == CLOCK_SET_SPEED) {
-                setTimer = 0;  //Clear the timer variable and start from the beginning
-                
-                timeMinutes++;  //Increment the minutes counter by 1 to indicate that a minute has passed
-                //Check to see if an hour has passed
-                if (timeMinutes == 60) {
-                    timeMinutes = 0;  //Reset the minutes counter back to 0 and begin counting for an hour again
-            
-                    timeHours++;  //Increment the hours counter by 1 to indicate that an hour has passed
-                    //Check to see if a day has passed
-                    if (timeHours == 24) {
-                        timeHours = 0;  //Reset the hours counter back to 0 and begin counting for a day again
-                    }
-                }
-                
-                updateDisplay();  //Update the display digits to represent the current time being tracked by the clock
-            }
-        }
-        
-        //Proceed only if the hour button is being pressed
-        if (RB4 && !RB5) {
-            setTimer++;  //Increment the timer used to delay each increment of the hours to make user interface much easier
-            
-            //Check to see if the timer has overflowed
-            if (setTimer == CLOCK_SET_SPEED) {
-                setTimer = 0;  //Clear the timer variable and start from the beginning
-                
-                timeHours++;  //Increment the hours counter by 1 to indicate that an hour has passed
-                //Check to see if a day has passed
-                if (timeHours == 24) {
-                    timeHours = 0;  //Reset the hours counter back to 0 and begin counting for a day again
-                }
-                
-                updateDisplay();  //Update the display digits to represent the current time being tracked by the clock
-            }
-        }
-        
-        //Reset the timer used to delay each increment of the time to make user interface easier if none of the buttons are being pressed
-        if (RB4 && RB5) {
-            setTimer = CLOCK_SET_SPEED - 1;  //Set the counter used to add a delay between increments of a time frame when setting the clocks time to the end of the cycle to start immediately
-        }
     }
 }
