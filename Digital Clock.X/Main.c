8ea86d81e3fc1a6634c212ed1817a6248cbea9e8
mikemadealarms <mike.stegne.host@gmail.com>
2016-07-13 17:01:10 -0400

    Updated to work on first prototype board
    Changed the way the display is multiplexed due to a new display.
    Made a some minor changes to a few registers and cleaned up the comments
    a small bit.

--- Digital Clock.X/Main.c
+++ Digital Clock.X/Main.c
@@ -2,8 +2,8 @@
  *  Digital Clock - A simple digital clock project to help me learn PIC  *
  *  Created by mikemadealarms on January 6, 2016 at 4:38 PM              *
  * --------------------------------------------------------------------- *
- *  Last modified by mikemadealarms on June 18, at 10:14 AM              *
- *  Last modification made was: Finalize and release first version       *
+ *  Last modified by mikemadealarms on June 13, 2016 at 2:31 PM          *
+ *  Last modification made was: Minor cleaning and test of 12 Hour       *
  *************************************************************************/
 
 #include <xc.h>
@@ -40,18 +40,18 @@
 #define TIME_SET_DELAY 0x7F;  //The lower the value, the faster the numbers will increment while using the buttons to set the time
 
 //Display Mapping
-unsigned char DISPLAY_MAPPING[] = {
-                                   0xB7,  //Digit mapping of 0
-                                   0x24,  //Digit mapping of 1
-                                   0x73,  //Digit mapping of 2
-                                   0x76,  //Digit mapping of 3
-                                   0xE4,  //Digit mapping of 4
-                                   0xD6,  //Digit mapping of 5
-                                   0xD7,  //Digit mapping of 6
+const unsigned char DISPLAY_MAPPING[] = {
+                                   0xFD,  //Digit mapping of 0
+                                   0x30,  //Digit mapping of 1
+                                   0x5E,  //Digit mapping of 2
+                                   0x3E,  //Digit mapping of 3
+                                   0x33,  //Digit mapping of 4
+                                   0x2F,  //Digit mapping of 5
+                                   0x6F,  //Digit mapping of 6
                                    0x34,  //Digit mapping of 7
-                                   0xF7,  //Digit mapping of 8
-                                   0xF6,  //Digit mapping of 9
-                                   0x08   //Digit mapping of the colon
+                                   0x7F,  //Digit mapping of 8
+                                   0x3F,  //Digit mapping of 9
+                                   0x80   //Digit mapping of the colon
 };
 
 /***************
@@ -59,9 +59,9 @@ unsigned char DISPLAY_MAPPING[] = {
  ***************/
 
 //Time Tracking Variables
-unsigned char timeSeconds = 0;  //Tracks the current half seconds of a minute and resets when a full second is completed
-unsigned char timeMinutes = 0;  //Tracks the current minute of the hour and resets when a full hour has passed
-unsigned char timeHours = 0;    //Tracks the current hour of the day and then resets when a day has passed
+unsigned char timeSeconds = 0x00;  //Tracks the current half seconds of a minute and resets when a full second is completed
+unsigned char timeMinutes = 0x00;  //Tracks the current minute of the hour and resets when a full hour has passed
+unsigned char timeHours = 0x00;    //Tracks the current hour of the day and then resets when a day has passed
 
 //Display Multiplexing Variables
 unsigned char displayDigit = 0;                                 //Represents the current digit in the display that is being multiplexed
@@ -74,12 +74,12 @@ unsigned char displayValue[] = {0x00, 0x00, 0x00, 0x00, 0x00};  //Holds the valu
 //Setup Function, configures MCU modules, initializes variables, and set IO registers
 void setup() {
     //Timing Related Registers
-    OSCCON = 0x70;         //Set the internal oscillator to run at 125kHz
+    OSCCON = 0x28;         //Set the internal oscillator to run at 125kHz
     OPTION_REG = 0x5F;     //Enable the internal weak pull-ups on PORTB and set the Timer0 module to run with no pre-scaler from the internal oscillator
     TMR1H = 0xFF;          //Set the highest byte of the Timer1 module counter
     TMR1L = 0xFF;          //Set the lowest byte of the Timer1 module counter
     T1CON = 0x8D;          //Enable the Timer1 module and set it to use an external low frequency crystal oscillator
-    T2CON = 0x7F;          //Enable the Timer2 module and set the prescaler to count on every 64th clock cycle
+    T2CON = 0x7F;          //Enable the Timer2 module and set the pre-scaler to count on every 64th clock cycle
     PR2 = TIME_SET_DELAY;  //Set the period register of Timer2 used to determine when an interrupt should occur to the value of the constant TIME_SET_DELAY 
     
     //Interrupt Related Registers
@@ -93,15 +93,15 @@ void setup() {
     WPUA = 0x00;    //Disable all the internal weak pull-up resistors on PORTA
     LATA = 0x00;    //Set all of PORTA to logic LOW
     
-    TRISB = 0xF0;   //Set PORTB0 to PORTB3 to outputs and leave the rest as inputs
+    TRISB = 0xE0;   //Set PORTB0 to PORTB4 to outputs and leave the rest as inputs
     ANSELB = 0x00;  //Set all of PORTB to digital IO
-    WPUB = 0x30;    //Enable the internal weak pull-ups on PORTB4 and PORTB5
+    WPUB = 0xE0;    //Enable the internal weak pull-ups on PORTB5, PORTB6, and PORTB7
     LATB = 0x00;    //Set all of PORTB to logic LOW
     
     //Initialize Variables
-    timeSeconds = 0;  //Clear the variable used for tracking seconds so it starts at the beginning
-    timeMinutes = 0;  //Clear the variable used for tracking minutes so it starts at the beginning
-    timeHours = 0;    //Clear the variable used for tracking hours so it starts at the beginning
+    timeSeconds = 0x00;  //Clear the variable used for tracking seconds so it starts at the beginning
+    timeMinutes = 0x00;  //Clear the variable used for tracking minutes so it starts at the beginning
+    timeHours = 0x00;    //Clear the variable used for tracking hours so it starts at the beginning
 }
 
 /****************
@@ -122,18 +122,21 @@ void main() {
 void updateClock() {
     timeSeconds++;  //Increment the seconds counter by 1 to indicate that half a second has passed
     //Check to see if a minute has passed
-    if (timeSeconds == 120) {
-        timeSeconds = 0;  //Reset the seconds counter back to 0 and begin counting for a minute again
+    
+    if (timeSeconds == 0x78) {
+        timeSeconds = 0x00;  //Reset the seconds counter back to 0 and begin counting for a minute again
         
         timeMinutes++;  //Increment the minutes counter by 1 to indicate that a minute has passed
+        
         //Check to see if an hour has passed
-        if (timeMinutes == 60) {
-            timeMinutes = 0;  //Reset the minutes counter back to 0 and begin counting for an hour again
+        if (timeMinutes == 0x3C) {
+            timeMinutes = 0x00;  //Reset the minutes counter back to 0 and begin counting for an hour again
             
             timeHours++;  //Increment the hours counter by 1 to indicate that an hour has passed
+            
             //Check to see if a day has passed
-            if (timeHours == 24) {
-                timeHours = 0;  //Reset the hours counter back to 0 and begin counting for a day again
+            if (timeHours == 0x18) {
+                timeHours = 0x00;  //Reset the hours counter back to 0 and begin counting for a day again
             }
         }
     }
@@ -144,30 +147,33 @@ void multiplexDisplay() {
     displayDigit++;  //Increment the digit counter up by 1 to indicate what digit is to be multiplexed
     
     //Check to see if the digit actually exists
-    if (displayDigit > 4) {
-        displayDigit = 0;  //Reset the digit counter back to 0 and start the multiplexing process over again
-        LATB = 0x00;       //Clear PORTB so that none of the digits on the display are enabled.
-    } else {
-        LATB = 1 << displayDigit - 1;  //Enable the display digit that currently being multiplexed to allow the segments to be written to it
+    if (displayDigit > 0x05) {
+        displayDigit = 0x00;  //Reset the digit counter back to 0 and start the multiplexing process over again
     }
     
-    LATA = ~displayValue[displayDigit];  //Write the segment values to the current digit being multiplexed
+    //Clear the display to prevent any ghosting of other digits on the display
+    LATA = 0x00;
+    LATB = 0x00;
+    
+    //Write the next digit to the display
+    LATA = displayValue[displayDigit];  //Write the segment values to the current digit being multiplexed
+    LATB = 0x01 << displayDigit;       //Enable the display digit that currently being multiplexed to allow the segments to be written to it
 }
 
 //Update Display Function, updates the data that is displayed on the display to be the same as the current time
 void updateDisplay() {
     //Toggle the colon
-    if (timeSeconds % 2) {
-        displayValue[0] = DISPLAY_MAPPING[10];  //Turn on the colon if the seconds value is even
+    if (timeSeconds % 0x02) {
+        displayValue[0x04] = DISPLAY_MAPPING[0x0A];  //Turn on the colon if the seconds value is even
     } else {
-        displayValue[0] = 0x00;  //Turn off the colon if the seconds value is odd
+        displayValue[0x04] = 0x00;  //Turn off the colon if the seconds value is odd
     }
     
     //Set the values of the digits on the display to be the current time
-    displayValue[1] = DISPLAY_MAPPING[timeHours % 100 / 10];    //Set the first digit of the display to display the tens place value of the current hour
-    displayValue[2] = DISPLAY_MAPPING[timeHours % 10];          //Set the second digit of the display to display the ones place value of the current hour
-    displayValue[3] = DISPLAY_MAPPING[timeMinutes % 100 / 10];  //Set the third digit of the display to display the tens place value of the current minute
-    displayValue[4] = DISPLAY_MAPPING[timeMinutes % 10];        //Set the fourth digit of the display to display the ones place value of the current minute
+    displayValue[0x03] = DISPLAY_MAPPING[timeHours % 0x64 / 0x0A / 2];    //Set the first digit of the display to display the tens place value of the current hour
+    displayValue[0x02] = DISPLAY_MAPPING[timeHours % 0x0A / 2];           //Set the second digit of the display to display the ones place value of the current hour
+    displayValue[0x01] = DISPLAY_MAPPING[timeMinutes % 0x64 / 0x0A / 2];  //Set the third digit of the display to display the tens place value of the current minute
+    displayValue[0x00] = DISPLAY_MAPPING[timeMinutes % 0x0A / 2];         //Set the fourth digit of the display to display the ones place value of the current minute
 }
 
 /****************
@@ -202,24 +208,24 @@ void interrupt onInterrupt() {
         TMR2IF = 0x00;  //Clear the Timer2 module match interrupt flag to prevent false interrupts
         
         //Check to see if the minutes button is being held down
-        if (!RB4 && RB5) {
+        if (RB6 && !RB7) {
             timeMinutes++;  //Increment the minutes counter by 1
             
             //Check to see if the minutes value is over an hour
-            if (timeMinutes == 60) {
-                timeMinutes = 0;  //Reset the minutes counter back to 0
+            if (timeMinutes == 0x3C) {
+                timeMinutes = 0x00;  //Reset the minutes counter back to 0
             }
             
             updateDisplay();  //Update the values on the display to reflect the newly set time
         }
         
         //Check to see if the hours button is being held down
-        if (RB4 && !RB5) {
+        if (!RB6 && RB7) {
             timeHours++;  //Increment the hours counter by 1
             
             //Check to see if the hours value is over a day
-            if (timeHours == 24) {
-                timeHours = 0;  //Reset the hours counter back to 0
+            if (timeHours == 0x18) {
+                timeHours = 0x00;  //Reset the hours counter back to 0
             }
             
             updateDisplay();  //Update the values on the display to reflect the newly set time
